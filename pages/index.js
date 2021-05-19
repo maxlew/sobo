4a7d671d83b1f826f559365c701b6e8359294d2c
Max Lewis <maxlew@gmail.com>
2021-05-20 09:09:57 +1000

    feat: cleanup and adding final copy

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import styled from 'styled-components';
 
+const breakpoint = '1024px'
+
 const Heading = styled.h1`
+  font-size: 42px;
   margin-top: 2em;
   text-align: center;
   font-family: 'Indie Flower', cursive;
@@ -10,15 +13,29 @@ const Heading = styled.h1`
 `;
 
 const Intro = styled.p`
-  padding: 2em;
+  padding: 1em 2em;
+  color: #fff;
+  mix-blend-mode: screen;
+  text-shadow: 2px 2px #000;
+
+  @media(min-width: ${breakpoint}) {
+    text-align: center;
+  }
+`
+
+const P = styled.p`
   color: #fff;
   mix-blend-mode: screen;
   text-shadow: 2px 2px #000;
 `
 
 const Section = styled.section`
-  padding: 2em;
-  padding-bottom: 4em;
+  display: flex;
+  flex-direction: column;
+
+  ${props => props.noPadding ? '' : 'padding: 2em;'}
+  ${props => props.double ? 'grid-column: span 2;' : ''}
+
   h2 {
     color: #fff;
     mix-blend-mode: screen;
@@ -27,7 +44,7 @@ const Section = styled.section`
 `
 
 const Gifts = styled.div`
-  @media (min-width: 600px) {
+  @media (min-width: ${breakpoint}) {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: max-content;
@@ -36,14 +53,24 @@ const Gifts = styled.div`
 
 const HeroImage = styled.img`
   width: 100%;
-  display: block;
-  max-height: 70vh;
-  object-fit: cover;
+  flex: 0;
+
+  @media(min-width: ${breakpoint}) {
+    max-height: 800px;
+    width: auto;
+    align-self: center;
+  }
 `
 
 const Image = styled.img`
   width: 100%;
   display: block;
+
+  ${props => props.mobileOnly && `
+    @media(min-width: ${breakpoint}) {
+      display: none;
+    }
+  `}
 `
 
 const DonateButton = styled.a`
@@ -64,7 +91,6 @@ const DonateButton = styled.a`
     transform: scale(1.2);
   }
 `
-
 export default function Home() {
   return (
     <div>
@@ -87,36 +113,46 @@ export default function Home() {
       </Head>
 
       <main>
-        <Heading>
-          Sophie and Ryan's<br/> Wedding Registry
-        </Heading>
-        <Intro>Hello! Thanks for visiting our page. Due to popular demand we have made this page for wedding gifts or contributions. Please bear in mind that we truly don’t need very much at all!</Intro>
-        <HeroImage src='/hats.jpg' />
-        <Intro>We have cobbled together some things or experiences we have been eyeing off or have planned if you would like to contribute.</Intro>
+        <Section double noPadding>
+          <Heading>
+            Sophie and Ryan's<br/> Wedding Registry
+          </Heading>
+          <Intro>Hello! Thanks for visiting our page & for being a guest at our wedding. This page was created due to popular demand. We truly do not need any gifts, but if you must, here are a few items or experiences you can contribute to.</Intro>
+          <HeroImage src='/3409a72e-e310-4555-b16b-79e64662399c.jpg' />
+          <Intro>We have cobbled together some things or experiences we have been eyeing off or have planned if you would like to contribute.</Intro>
+        </Section>
         <Gifts>
           <Section>
             <h2>Sound Bar</h2>
+            <P>We have been thinking about an upgrade to our sound set up for our future life as married netflix couch potatoes. And are looking at a Bose set up, naturally.</P>
             <Image src='https://media3.giphy.com/media/Me15sgUoYSCjHCs6wI/giphy.gif?cid=790b761149b8e4ec877657492f6fedfe2cb8cf4a669077c0&rid=giphy.gif&ct=g' />
             <Image src='https://media0.giphy.com/media/efUcLJePY6RkA/giphy.gif?cid=ecf05e47xhah19luk9zcmaxpwsjvw6gw12jrjlc66v9wuxna&rid=giphy.gif&ct=g' />
-            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Give money for sick sound</DonateButton>
+            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Give money towards sick sound</DonateButton>
           </Section>
           <Section>
             <h2>Honeymoon</h2>
+            <P>We are road tripping to the Grampians & Otways in the depths of winter, staying in Halls Gap & Port Campbell. Contribute to our accommodation or our amenities budget!</P>
             <Image src='https://media0.giphy.com/media/CMrWv83j6Kfio/giphy.gif?cid=ecf05e47oyyy7r50xy5pkm8ndkllsi9ht9u52x4q9k5ut3nj&rid=giphy.gif&ct=g' />
             <Image src='/scream.jpg' />
-            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Give money for Holidays</DonateButton>
+            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Help us on our Honeymoon</DonateButton>
           </Section>
           <Section>
-            <h2>Something Else</h2>
+            <h2>Household goods</h2>
+            <P>We have lots of items already, and Ryan's favourite pastime is shopping. We do like luxury items like Sheridan sheet sets. Which are too much for one or two guests to buy. Contribute to a set!</P>
             <Image src='https://media4.giphy.com/media/5GoVLqeAOo6PK/giphy.gif?cid=ecf05e479apmas4xudfy33l5rsedwqyb2t6unvpudgpqr5ek&rid=giphy.gif&ct=g' />
             <Image src='https://media0.giphy.com/media/xUOwFYTPn5m0eEca64/giphy.gif?cid=ecf05e47pg7rn1ql27eogxq06nj4vvind88ctu5oeyfol1h9&rid=giphy.gif&ct=g' />
-            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Give money for random shit</DonateButton>
+            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Donate to our Palace</DonateButton>
           </Section>
           <Section>
             <h2>Treats for Norma</h2>
+            <P>Norma is a big girl. She consumes lots of food, and plays lots of games. She loves food and toys so any contributions here will be enthusiastically received.</P>
             <Image src='/norma-blur.png' />
-            <Image src='https://media2.giphy.com/media/huUcR3h1IpRkNJucgx/giphy.gif?cid=ecf05e47pg2f8fifea3mep1fpxc4yavzg6v6wiqkersa85dz&rid=giphy.gif&ct=g' />
-            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Give money for norms</DonateButton>
+            <Image mobileOnly src='https://media2.giphy.com/media/huUcR3h1IpRkNJucgx/giphy.gif?cid=ecf05e47pg2f8fifea3mep1fpxc4yavzg6v6wiqkersa85dz&rid=giphy.gif&ct=g' />
+            <DonateButton href="https://paypal.me/pools/c/8zsfktBS5c">Give treats to Norms</DonateButton>
+          </Section>
+          <Section double>
+            <Image src='/IMG_1795.GIF' />
+            <P>Thanks! Love you & see you June 26th x</P>
           </Section>
         </Gifts>
         <div className="background" />
